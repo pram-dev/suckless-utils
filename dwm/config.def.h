666f1b966469aa4f0c46a9a334485a9d4fb8da20
notpramdev <notpramdev@gmail.com>
2023-06-11 03:39:41 -0700

    Update colors

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -17,7 +17,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const double defaultopacity  = 1.00;
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Medium:size=12.0" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Medium:size=12.0";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#111111";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
