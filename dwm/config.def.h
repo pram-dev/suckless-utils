bbffcbddab0bfe87f155ce0f9734107ba618f512
notpramdev <notpramdev@gmail.com>
2023-06-11 17:40:33 -0700

    Update text color

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -20,13 +20,13 @@ static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Medium:size
 static const char col_gray1[]       = "#111111";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#37474F"; // fedora blue color #072c61, original #005577
 static const unsigned int baralpha = 0xff; // originally set to '0xd0'
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray1 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 static const unsigned int alphas[][3]      = {
