9e792bfdbee2d919c1f3432e4e14aeba705071b0
notpramdev <notpramdev@gmail.com>
2023-06-14 23:08:00 -0700

    Update fonts

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -16,8 +16,8 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const double defaultopacity  = 1.00;
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Medium:size=12.0" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Medium:size=12.0";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Bold:size=12.0" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Bold:size=12.0";
 static const char col_gray1[]       = "#111111";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
