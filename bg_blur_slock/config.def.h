201be283111b754f2e3a0966c77c34869d84b2fc
notpramdev <notpramdev@gmail.com>
2023-06-15 03:23:38 -0700

    Add pixelation and slightly increase blur radius

--- bg_blur_slock/config.def.h
+++ bg_blur_slock/config.def.h
@@ -31,8 +31,8 @@ static XRectangle rectangles[9] = {
 /*Enable blur*/
 #define BLUR
 /*Set blur radius*/
-static const int blurRadius=5;
+static const int blurRadius=8;
 /*Enable Pixelation*/
-//#define PIXELATION
+#define PIXELATION
 /*Set pixelation radius*/
-static const int pixelSize=0;
+static const int pixelSize=8;
